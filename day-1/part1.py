ff1760ea50dc5245d5d3ee6f546bdcd951a3dba4
Robin Bakker <mail@rsbakker.nl>
2020-12-01 15:08:59 +0100

    day1

---
+++ day-1/part1.py
@@ -0,0 +1,12 @@
+# Get input file
+with open("./day-1/input.txt") as f:
+    input = f.readlines()
+input = [x.strip() for x in input]
+input = [ int(x) for x in input ]
+
+# Find the two entries that sum to 2020 and multiply them by eachother
+for i in input:
+    for j in input:
+        if(i + j == 2020):
+            print(i*j)
+            exit()
---
+++ day-1/part2.py
@@ -0,0 +1,15 @@
+# Get input file
+with open("./day-1/input.txt") as f:
+    input = f.readlines()
+input = [x.strip() for x in input]
+input = [ int(x) for x in input ]
+
+# Find the three entries that sum to 2020 and multiply them by eachother
+for i in input:
+    for j in input:
+        for k in input:
+            if(i + j + k == 2020):
+                print(i*j*k)
+                exit()
+
+# This is not efficient as it is O(n^3), but it works!
