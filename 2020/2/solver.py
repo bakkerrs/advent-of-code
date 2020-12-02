1fef8ff1fd0e310be10867c0e1f0220d73c5e10d
Robin Bakker <mail@rsbakker.nl>
2020-12-02 14:04:47 +0100

    Better way of handling input splitting - regex

--- 2020/2/solver.py
+++ 2020/2/solver.py
@@ -1,12 +1,12 @@
+import re
+
 def get_input():
     with open("./2020/2/input.txt") as f:
         return f.read().split("\n")
 
 def split_input(input):
-    policy, password = [x.strip() for x in input.split(":")]
-    bounds, token = policy.split(" ")
-    lower, upper = [int(x) for x in bounds.split("-")]
-    return(lower, upper, token, password)
+    lower, upper, token, password = re.split("[- :]+", input)
+    return(int(lower), int(upper), token, password)
 
 def part1(input):
     i = 0
@@ -15,7 +15,7 @@ def part1(input):
         count = password.count(token)
         if(count >= lower and count <= upper):
             i += 1
-    print(i)
+    return(i)
 
 def part2(input):
     i = 0
@@ -26,11 +26,11 @@ def part2(input):
         if((a == token) ^ (b == token)):
             i += 1
     
-    print(i)
+    return(i)
 
 def main():
-    part1(get_input())
-    part2(get_input())
+    print(part1(get_input()))
+    print(part2(get_input()))
     pass
 
 main()
